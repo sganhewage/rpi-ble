237087653a5ac981898cf1d356a5c3f6edfe7dfc
sganhewage <sganhewage@guilford.edu>
2025-06-26 15:09:40 -0400

    added shell command

--- listDevicesUSB.py
+++ listDevicesUSB.py
@@ -1,6 +1,25 @@
 import pyvisa
+import subprocess
 
 def list_usb_devices():
+    try:
+        subprocess.run(["sudo udevadm control --reload"], check=True)
+    except Exception as e:
+        print(f"Firmware load failed or not needed: {e}")
+    
+    
+    try:
+        subprocess.run(["sudo /usr/sbin/gpib_config"], check=True)
+    except Exception as e:
+        print(f"gpib_config failed: {e}")
+        
+    for i in range(10):
+        if subprocess.call(["test", "-e", "/dev/gpib0"]) == 0:
+            break
+        time.sleep(1)
+    else:
+        raise RuntimeError("/dev/gpib0 did not appear")
+    
     """List all USB devices connected to the system."""
     rm = pyvisa.ResourceManager()
     devices = rm.list_resources()
